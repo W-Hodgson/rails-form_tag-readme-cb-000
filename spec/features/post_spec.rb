4fa683f0452d8d165b3f9d1177761bfa3041fc22
Will Hodgson <will.hodgson@current-rms.com>
2019-10-21 08:17:24 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'new_post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
-    expect(page.status_code)
+    expect(page.status_code).to eq
   end
 end
