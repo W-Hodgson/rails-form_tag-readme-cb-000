f8cd86b2a81ab921e1e8a3dfb3d81f940d644e6c
Will Hodgson <will.hodgson@current-rms.com>
2019-10-21 08:41:25 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -20,6 +20,6 @@ describe 'new_post' do
 
     expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
-    expect(page).to 
+    expect(page).to have_content()
   end
 end
