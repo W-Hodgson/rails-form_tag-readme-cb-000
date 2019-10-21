4915a728dd5a4fba396e44935bed711c7902f65f
Will Hodgson <will.hodgson@current-rms.com>
2019-10-21 08:40:23 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,6 +13,6 @@ describe 'new_post' do
 
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
     visit new_post_path
-    fill_in 'post_title'
+    fill_in 'post_title', with: 'My post title'
   end
 end
